bed10e059f83433d8c743f5734e119880f134df9
fpereira-epfl <francisco.pinto@epfl.ch>
2025-10-31 23:59:13 +0100

    Remove file.

--- start.sh
+++
@@ -1,5 +0,0 @@
-# Method 1: Using Python script to start the server
-python -m languageninja.api.main
-
-# Method 2: Using Uvicorn command with --reload flag
-# python -m uvicorn languageninja.api.main:app --reload
