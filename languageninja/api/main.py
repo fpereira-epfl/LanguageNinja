a76f2c7e1f4106c6aec48fc0d13411c863eeb9a9
fpereira-epfl <francisco.pinto@epfl.ch>
2025-10-31 18:31:55 +0100

    Added initialisation based on env variables.

--- languageninja/api/main.py
+++ languageninja/api/main.py
@@ -1,9 +1,11 @@
 # main.py
+from languageninja.api.router import api
 from pathlib import Path
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-from languageninja.api.router import api
 from fastapi.staticfiles import StaticFiles
+from dotenv import load_dotenv
+import os, uvicorn
 
 APP_DIR = Path(__file__).resolve().parent
 FRONTEND = "ui"
@@ -16,3 +18,36 @@ app.mount("/audio", StaticFiles(directory=APP_DIR.parent.parent / "data" / "audi
 @app.get("/")
 def root():
     return FileResponse(INDEX_FILE)
+
+# This block allows you to run the file directly
+# and have the Uvicorn server start with your specified host and port.
+if __name__ == "__main__":
+
+    # Load environment variables from .env
+    load_dotenv()
+
+    # Retrieve host and port from .env
+    env_host = os.getenv("HOST")
+    env_port = os.getenv("PORT")
+    env_reload = os.getenv("RELOAD")
+
+    # Enforce int
+    env_port = int(env_port) if env_port else None
+
+    # Validate reload
+    if env_reload is not None:
+        env_reload = env_reload.lower() in ("true", "1", "yes")
+    else:
+        env_reload = False
+
+    # Validate host and port
+    if not env_host or not env_port:
+        raise ValueError("HOST and/or PORT not found in .env file")
+
+    # Start Uvicorn server with specified host and port
+    uvicorn.run(
+        "languageninja.api.main:app",
+        host=env_host,
+        port=env_port,
+        reload=env_reload  # Optional: For development, to reload on code changes
+    )
--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
-python -m uvicorn languageninja.api.main:app --reload
+# Method 1: Using Python script to start the server
+python -m languageninja.api.main
+
+# Method 2: Using Uvicorn command with --reload flag
+# python -m uvicorn languageninja.api.main:app --reload
